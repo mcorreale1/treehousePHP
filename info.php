19ecbc01c53e9f525178430941b3bf8e784f8875
mcorreale1 <mcorreale1@gmail.com>
2015-01-07 00:28:14 -0500

    First commit of 2015! I'm way too lazy at home

--- info.php
+++ info.php
@@ -6,17 +6,29 @@ $pageTitle="Information";
 $section="info";
 include(ROOTPATH.'inc/header.php');
 ?>
-
+`
 	<div class="section page">
 		<div class="wrapper">
 			<h1>Information</h1>
+            
+            <pre>
+                <?php
+                    //Testing STR Pos string
+                    echo strpos("cookie dough","oo");
+                ?>
+            </pre>
+            
+            
 		</div>
 	</div>
 
-	<script>
+	
+
+
+<script>
 
 	$(document).ready(function(){
-	 	$(".footer").hover(function(){
+	 	$(".h1").mouseenter(function(){
 	   	 	$(this).hide();
 	  	},function(){
 	  		$(this).show();
